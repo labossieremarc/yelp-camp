f027deaa97db94317a8cff233142b738a1e4c395
unknown <labossieremarc@gmail.com>
2021-02-02 09:24:00 -0700

    change

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -3,7 +3,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: '873163186136771',
+    api_key: process.env.CLOUDINARY_KEY,
     api_secret: 'mKQyDhgPyLoKyxh2UfFKd0ieu40'
 })
 
