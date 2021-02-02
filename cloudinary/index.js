90dc85c704931375f652a6c6325e6a4c72e665c7
unknown <labossieremarc@gmail.com>
2021-02-02 09:16:11 -0700

    change

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_Key,
-    api_secret: process.env.CLOUDINARY_SECRET
+    cloud_name: 'midnight',
+    api_key: '873163186136771',
+    api_secret: 'mKQyDhgPyLoKyxh2UfFKd0ieu40'
 })
 
 const storage = new CloudinaryStorage({
